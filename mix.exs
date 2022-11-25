4628903c50722128b2c2514de88233d8e1e8e606
BigSpaces <113518695+BigSpaces@users.noreply.github.com>
2022-11-25 14:46:40 +0100

    Updated mix.exs

--- mix.exs
+++ mix.exs
@@ -4,6 +4,9 @@ defmodule Pokelixir.MixProject do
   def project do
     [
       app: :pokelixir,
+      name: "Pokelixir",
+      source_url: "https://github.com/BigSpaces/pokelixir",
+      homepage_url: "https://github.com/BigSpaces/pokelixir",
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
@@ -11,7 +14,6 @@ defmodule Pokelixir.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,7 +21,6 @@ defmodule Pokelixir.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
